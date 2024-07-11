8a5e109556755db3a37fd40c1e5bae5613242a87
Jonathan Menino <jmeninodesign@gmail.com>
2024-07-11 17:09:05 -0300

    Melhorei meu construtor inserindo a classe.

--- script.js
+++ script.js
@@ -1,55 +1,72 @@
 //CALCULADORA DE ORÇAMENTO
 //O OBJETIVO DESTE SISTEMA É CALCULAR O PREÇO QUE UM EMPREENDEDOR PRECISA COBRAR EM UM SERVIÇO.
 
-alert ("CALCULADORA DE ORÇAMENTO. O OBJETIVO DESTE SISTEMA É CALCULAR O PREÇO QUE UM EMPREENDEDOR PRECISA COBRAR EM UM SERVIÇO.")
+alert(
+  "CALCULADORA DE ORÇAMENTO. O OBJETIVO DESTE SISTEMA É CALCULAR O PREÇO QUE UM EMPREENDEDOR PRECISA COBRAR EM UM SERVIÇO."
+);
 
 //CÁLCULAR O CUSTO HORA DA EMPRESA
 const custoHora = (despesas, salario, horas) => {
-    return (despesas + salario) / horas;
-}
+  return (despesas + salario) / horas;
+};
 
 //CALCULAR O VALOR BRUTO DO SERVIÇO
 const valorBrutoServico = (custoHora, horasServico) => {
-    return custoHora * horasServico;
-}
+  return custoHora * horasServico;
+};
 
 //CALCULAR O VALOR FINAL DO SERVIÇO
 const valorFinalServico = (valorBrutoServico, percentualNegociacao) => {
-    return ((valorBrutoServico * percentualNegociacao) / 100) + valorBrutoServico;
-}
-
+  return (valorBrutoServico * percentualNegociacao) / 100 + valorBrutoServico;
+};
 
 //COLETAR INFORMAÇÕES DA EMPRESA
-const valorDespesas = parseInt(prompt("Digite o valor total das despesas mensais do seu negócio."));
+const valorDespesas = parseInt(
+  prompt("Digite o valor total das despesas mensais do seu negócio.")
+);
 const valorSalario = parseInt(prompt("Digite o seu salário desejado mensal."));
-const qtdHoras = parseInt(prompt("Digite a quantidade de horas mensais que você deseja trabalhar."));
+const qtdHoras = parseInt(
+  prompt("Digite a quantidade de horas mensais que você deseja trabalhar.")
+);
 
 const empresa = {
-    despesasMensais: valorDespesas,
-    salarioMensal: valorSalario,
-    horasMensais: qtdHoras, 
-    custoHora: custoHora(valorDespesas, valorSalario, qtdHoras)
-}
+  despesasMensais: valorDespesas,
+  salarioMensal: valorSalario,
+  horasMensais: qtdHoras,
+  custoHora: custoHora(valorDespesas, valorSalario, qtdHoras),
+};
 
 //COLETAR INFORMAÇÕES DO SERVIÇO
-const horasServico = parseInt(prompt(`O valor hora da sua empresa é de ${empresa.custoHora.toFixed(2)}. Agora digite quantas horas você estima que levará para concluir o serviço.`));
-const percentualNegociacao = parseInt(prompt(`Por último, adicione o percentual % de margem de negociação que deseja adicionar no valor final (SEM O CARACTERE %).`));
-
+const horasServico = parseInt(
+  prompt(
+    `O valor hora da sua empresa é de ${empresa.custoHora.toFixed(
+      2
+    )}. Agora digite quantas horas você estima que levará para concluir o serviço.`
+  )
+);
+const percentualNegociacao = parseInt(
+  prompt(
+    `Por último, adicione o percentual % de margem de negociação que deseja adicionar no valor final (SEM O CARACTERE %).`
+  )
+);
 
 //FUNÇÃO CONSTRUTORA DO OBJETO SERVIÇO
-function criarServico(horas,percentual) {
+class Servico {
+  constructor(horas, percentual) {
     this.horas = horas;
     this.percentual = percentual;
-    this.totalTaxas = function() {
-        return (horas * empresa.custoHora) * (percentual / 100);
-    }
-    this.valorFinal = function() {
-        return (horas * empresa.custoHora) + this.totalTaxas();
-    }
+    this.totalTaxas = function () {
+      return horas * empresa.custoHora * (percentual / 100);
+    };
+    this.valorFinal = function () {
+      return horas * empresa.custoHora + this.totalTaxas();
+    };
+  }
 }
 
 //CHAMANDO A FUNÇÃO CONSTRUTORA
-const servico = new criarServico(horasServico,percentualNegociacao)
+const servico = new Servico(horasServico, percentualNegociacao);
 
-
-alert(`O valor total do seu orçamento é de R$ ${servico.valorFinal().toFixed(2)}.`);
+alert(
+  `O valor total do seu orçamento é de R$ ${servico.valorFinal().toFixed(2)}.`
+);
